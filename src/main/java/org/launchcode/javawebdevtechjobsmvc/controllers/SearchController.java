1d2ffd61b5d1afc48dfb95ba61e24fb459ff01a2
Kayla Jones <kayla3445@yahoo.com>
2021-02-18 21:37:44 -0600

    finished assignment

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -25,4 +27,26 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("results")
+    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        ArrayList<Job> allJobs = JobData.findAll();
+        ArrayList<Job> jobSearchList = new ArrayList<>();
+        if(searchType.equals("all") || searchType.isEmpty()) {
+            // search all jobs for search term and add to jobSearchList
+            for(int i=0; i <allJobs.size(); i++) {
+                String search = allJobs.get(i).toString().toLowerCase();
+                if (search.contains(searchTerm.toLowerCase())) {
+                    jobSearchList.add(allJobs.get(i));
+                }
+            }
+            model.addAttribute("jobList", jobSearchList);
+        } else {
+        // send search term and search type into findByColumnAndValue and store in arrayList
+            jobSearchList = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobList", jobSearchList);
+
+        }
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
 }
